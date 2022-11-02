d91ebed788e34c1865731d8b17a8c508f9429cef
mecaneer23 <mecaneer23@gmail.com>
2022-11-02 16:20:32 -0500

    Make stuff not throw errors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bots/PreBot.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.Bots;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Base.Robot;
+import org.firstinspires.ftc.teamcode.Components.Arm;
+import org.firstinspires.ftc.teamcode.Components.Grabber;
+import org.firstinspires.ftc.teamcode.Components.AutoMecanum;
+
+@TeleOp
+public class PreBot extends Robot {
+    public AutoMecanum mecanum;
+    public Arm arm;
+    public Grabber grabber;
+
+    @Override
+    protected void initBot(HardwareMap hardwareMap, Telemetry telemetry, LinearOpMode opMode) {
+
+        this.mecanum = new AutoMecanum(
+                opMode,
+                "frontLeft",
+                "frontRight",
+                "backLeft",
+                "backRight",
+                hardwareMap,
+                telemetry,
+                1,
+                1,
+                10.5,
+                12.5,
+                1.13,
+                100,
+                false,
+                0,
+                0,
+                0
+        );
+        this.arm = new Arm("arm", hardwareMap, telemetry);
+        this.grabber = new Grabber("grabber", hardwareMap, telemetry);
+
+        addComponents(mecanum, arm, grabber);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/mainOp.java
@@ -0,0 +1,85 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Base.BaseOpMode;
+import org.firstinspires.ftc.teamcode.Bots.PreBot;
+
+public class mainOp extends BaseOpMode {
+    PreBot robot = new PreBot();
+
+    @Override
+    public void onUpdate() {
+       // drivetrain
+        if (gamepad1.dpad_up) {
+            robot.mecanum.mecanum.driveForward();
+        } else if (gamepad1.dpad_down) {
+            robot.mecanum.mecanum.driveBackward();
+        }
+
+        if (gamepad1.dpad_right) {
+            robot.mecanum.mecanum.strafeRight();
+        } else if (gamepad1.dpad_left) {
+            robot.mecanum.mecanum.strafeLeft();
+        }
+
+        if (gamepad1.back) {
+            robot.mecanum.mecanum.turnLeft();
+        } else if (gamepad1.guide) {
+            robot.mecanum.mecanum.turnRight();
+        }
+
+        robot.mecanum.setInitialDirections(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+
+        if (gamepad1.right_bumper) {
+            robot.mecanum.setHalfSpeed();
+        } else if (gamepad1.left_bumper) {
+            robot.mecanum.setQuarterSpeed();
+        } else if (gamepad1.start) {
+            robot.mecanum.buttonPressed();
+        } else {
+            robot.mecanum.buttonReleased();
+        }
+
+        robot.mecanum.update();
+
+        if (gamepad1.left_stick_button) {
+            robot.mecanum.mecanum.turnLeft();
+        } else if (gamepad1.right_stick_button) {
+            robot.mecanum.mecanum.turnRight();
+        }
+
+        // arm
+        if (gamepad2.left_bumper) {
+            robot.arm.move(robot.arm.ZERO_POSITION);
+        } else if (gamepad2.a) {
+            robot.arm.move(robot.arm.LOW_JUNCTION);
+        } else if (gamepad2.b) {
+            robot.arm.move(robot.arm.MEDIUM_JUNCTION);
+        } else if (gamepad2.y) {
+            robot.arm.move(robot.arm.HIGH_JUNCTION);
+        } else if (gamepad2.right_bumper) {
+            robot.arm.move(robot.arm.UPPER_BOUND);
+        } else if (gamepad2.right_trigger > 0 || gamepad2.left_trigger > 0) {
+            if (robot.arm.getCurrentPosition() < robot.arm.LOWER_BOUND) {
+                robot.arm.move(robot.arm.LOWER_BOUND + 10);
+            } else if (robot.arm.getCurrentPosition() > robot.arm.UPPER_BOUND) {
+                robot.arm.move(robot.arm.UPPER_BOUND - 10);
+            } else {
+                robot.arm.move((int) ((gamepad2.right_trigger - gamepad2.left_trigger) * 100) + robot.arm.getCurrentPosition(), gamepad2.right_trigger - gamepad2.left_trigger);
+            }
+        }
+//        new Action(arm::update, true);
+        robot.arm.update();
+
+        // grabber
+        if (gamepad2.x) {
+            robot.grabber.buttonPressed();
+        } else {
+            robot.grabber.buttonReleased();
+        }
+//        new Action(grabber::update, true);
+        robot.grabber.update();
+
+        telemetry.update();
+    }
+}
+
