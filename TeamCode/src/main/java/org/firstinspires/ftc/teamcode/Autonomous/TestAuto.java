fd00b890a3178393b33baa55ebdfae2e9c7cf986
mecaneer23 <mecaneer23@gmail.com>
2022-11-04 14:04:08 -0500

    use Utils/Action instead of custom class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TestAuto.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.teamcode.Base.BaseOpMode;
 import org.firstinspires.ftc.teamcode.Base.Robot;
 import org.firstinspires.ftc.teamcode.Bots.PreBot;
+import org.firstinspires.ftc.teamcode.Utils.Action;
 
 @Autonomous
 public class TestAuto extends BaseOpMode {
@@ -23,29 +24,12 @@ public class TestAuto extends BaseOpMode {
 
     @Override
     public void onStart() throws InterruptedException {
-        //almost working
-        Runnable r = new MoveArm(robot);
-        new Thread(r).start();
+        new Thread(
+            new Action(() -> {
+                robot.arm.move(robot.arm.HIGH_JUNCTION);
+            }, true)
+        ).start();
         robot.mecanum.driveForward(24);
 
-}
-
-
-
-
-
-}
-//Runnable instead of Thread lets us pass parameters
-class MoveArm implements Runnable {
-    PreBot robot;
-
-    public MoveArm(PreBot robot) {
-        this.robot = robot;
-
-    }
-
-    public void run()
-    {
-        robot.arm.move(robot.arm.HIGH_JUNCTION);
     }
 }
